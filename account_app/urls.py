515f31dcb45dfd7def3a54cdc68dd76aadfddd15
Amin-py69 <amin2004mohammad@outlook.com>
2023-11-14 18:15:50 +0330

    add dynamic url

--- account_app/urls.py
+++ account_app/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . import views
 
+
+app_name = "account_app"
 urlpatterns = [
-    path('login', views.login_user),
-    path('logout', views.logout_user),
-    path('register', views.register_user),
+    path('login', views.login_user, name='login'),
+    path('logout', views.logout_user, name='logout'),
+    path('register', views.register_user, name='register'),
 ]
