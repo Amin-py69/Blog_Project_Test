7f82ac8dbef5d23f808514a57aae984ed4e2d0b2
Amin-py69 <amin2004mohammad@outlook.com>
2023-12-07 10:45:48 +0330

    create post list html and url and in home page and url

--- blog_app/views.py
+++ blog_app/views.py
@@ -7,3 +7,8 @@ from .models import Article
 def post_detail(request, slug):
     article = Article.objects.get(slug=slug)
     return render(request, 'blog_app/post-details.html', {'article': article})
+
+
+def post_list(request):
+    articles = Article.filter_manager.published()
+    return render(request, 'blog_app/post-list.html', {'articles': articles})
